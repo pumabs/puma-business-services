62832573cfeb889107661125d205232eff07f958
pumabs <daniel@puma-bs.co.uk>
2026-09-01 11:22:02 +0100

    Add CookieConsent component to layout

--- app/layout.tsx
+++ app/layout.tsx
@@ -3,6 +3,7 @@ import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
 import Header from "./components/Header";
 import Footer from "./components/Footer";
+import CookieConsent from "./components/CookieConsent";
 
 const geistSans = Geist({
   variable: "--font-geist-sans",
@@ -163,6 +164,7 @@ export default function RootLayout({
         </main>
 
         <Footer />
+        <CookieConsent />
       </body>
     </html>
   );
