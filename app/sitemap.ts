60e85009a07b1aa5c9008e899ac8ad04b709a402
pumabs <daniel@puma-bs.co.uk>
2026-09-01 11:39:24 +0100

    Add cookies URL to sitemap

--- app/sitemap.ts
+++ app/sitemap.ts
@@ -46,5 +46,11 @@ export default function sitemap(): MetadataRoute.Sitemap {
       changeFrequency: "yearly",
       priority: 0.3,
     },
+    {
+  url: `${baseUrl}/cookies`,
+  lastModified: new Date(),
+  changeFrequency: "yearly",
+  priority: 0.3,
+},
   ];
 }
